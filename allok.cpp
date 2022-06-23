22b7647656f04cb00e13853a77fd9aef96d7e940
MikolajHanusz <92816085+MikolajHanusz@users.noreply.github.com>
2022-06-23 15:21:08 +0200

    Add files via upload

---
+++ allok.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+enum class typ
+{
+	asd, asdy
+};
+typ* alokuj(int s)
+{
+	typ* wsk = new typ[s];
+	return wsk;
+}
+void dealokuj(typ** wsk)
+{
+	delete[] *wsk;
+	*wsk = nullptr;
+	std::cout << *wsk << std::endl;
+}
+
+int main()
+{
+	int n;
+	std::cin >>n;
+	typ* wsk = alokuj(n);
+	std::cout << wsk << std::endl;
+	std::cin >> n;
+	dealokuj(&wsk);
+	std::cin >> n;
+	std::cout << wsk << std::endl;
+
+}
